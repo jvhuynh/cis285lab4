ac8631182c2ab1df8a443ab6ac0f27c7545be117
jvhuynh <39070396+jvhuynh@users.noreply.github.com>
2020-02-06 13:33:09 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,6 @@ public static void main(String[] args)
 for (int i =1; i <= 10; i++)
 {
 system.out.println("Chewing loudly is a no no" +i);
-system.out.println("Constantly popping bubblegum is also a no no")
 }
 }
 }
