e77ec45de02271490b4caa60fd97c53ed8f920c6
jvhuynh@umich.edu <39070396+jvhuynh@users.noreply.github.com>
2020-02-06 14:47:11 -0500

    fixed package system by changing "s" to "S"

--- Hello.java
+++ Hello.java
@@ -3,11 +3,11 @@ public static void main(String[] args)
 {
 for (int i =1; i <= 10; i++)
 {
-system.out.println("Chewing loudly is a no no" +i);
-system.out.println("A person's mouth should be close when they are chewing", +i);
-system.out.println("And here we go again", i++);
-system.out.println("Let's try again", i++);
-system.out.println("Another try", i++);
+System.out.println("Chewing loudly is a no no" +i);
+System.out.println("A person's mouth should be close when they are chewing", +i);
+System.out.println("And here we go again", i++);
+System.out.println("Let's try again", i++);
+System.out.println("Another try", i++);
 }
 }
 }
