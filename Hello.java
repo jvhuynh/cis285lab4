78edb9941a4cd5ce7b7ebccadbfd286ef47fac11
jvhuynh@umich.edu <39070396+jvhuynh@users.noreply.github.com>
2020-02-06 14:51:33 -0500

    so many changes

--- Hello.java
+++ Hello.java
@@ -3,11 +3,8 @@ public static void main(String[] args)
 {
 for (int i =1; i <= 10; i++)
 {
-System.out.println("Chewing loudly is a no no"+i);
+System.out.println("Chewing loudly is a no no", +i);
 System.out.println("A person's mouth should be close when they are chewing", +i);
-System.out.println("And here we go again", +i);
-System.out.println("Let's try again", +i);
-System.out.println("Another try", +i);
 }
 }
 }
