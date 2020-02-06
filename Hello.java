9b9197f79e337bd29736a6e158740a9abd4c2b9c
jvhuynh@umich.edu <39070396+jvhuynh@users.noreply.github.com>
2020-02-06 13:56:10 -0500

    Commit

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 for (int i =1; i <= 10; i++)
 {
 system.out.println("Chewing loudly is a no no" +i);
+system.out.println("A person's mouth should be close when they are chewing", +i);
 }
 }
 }
